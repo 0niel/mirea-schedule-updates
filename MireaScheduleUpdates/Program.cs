003409cd8ef6c05b6d00da4f857b2ef305834ec1
Anastasia Karpova <vip.anas2003@inbox.ru>
2024-02-01 17:52:45 +0300

    date txt ignored and damn directory please work

--- MireaScheduleUpdates/Program.cs
+++ MireaScheduleUpdates/Program.cs
@@ -1,5 +1,16 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+string path = @"date.txt";
+string text = DateTime.Now.ToString();
+using (FileStream fstream = new(path, FileMode.OpenOrCreate))
+{
+    byte[] buffer = Encoding.Default.GetBytes(text);
+    await fstream.WriteAsync(buffer);
+}
+
 
 app.MapGet("/", () => "Hello World!");
 
