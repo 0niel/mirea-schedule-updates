f63013040ef55e1dce64638ec60e18d6242e0fb6
Anastasia Karpova <vip.anas2003@inbox.ru>
2024-02-01 18:49:18 +0300

    Web App to Console

--- MireaScheduleUpdates/Program.cs
+++ MireaScheduleUpdates/Program.cs
@@ -1,19 +1,12 @@
-using System.Text;
+﻿using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-string path = @"date.txt";
+string path = @"../../../date.txt";
 string text = DateTime.Now.ToString();
 using (FileStream fstream = new(path, FileMode.OpenOrCreate))
 {
+    Console.WriteLine(text);
     byte[] buffer = Encoding.Default.GetBytes(text);
     await fstream.WriteAsync(buffer);
 }
 
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
-
-await app.StopAsync();
+Task.Delay(1000).Wait();
